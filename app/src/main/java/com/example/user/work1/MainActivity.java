2261a764baf84b25e978a4ede50c9fd72c108cbb
gdmec07150916 <1508930639@qq.com>
2017-01-02 20:58:06 +0800

    asdf

---
+++ app/src/main/java/com/example/user/work1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.work1;
+
+import android.media.MediaActionSound;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
